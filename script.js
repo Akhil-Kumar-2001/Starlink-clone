e03aa8330538c3e8601f358b77df8c29665290cd
Akhil Kumar <akhilkumar2001@gmail.com>
2024-03-14 10:19:30 +0530

    project completed

---
+++ script.js
@@ -0,0 +1,13 @@
+function showRightsidebar()
+ {
+   const rightsidebar=document.querySelector(".Rightsidebar");
+   rightsidebar.style.display="flex";
+//    
+ }
+ function hideRightsidebar()
+ {
+    const rightsidebar=document.querySelector(".Rightsidebar");
+    rightsidebar.style.display="none";
+    
+
+ }
